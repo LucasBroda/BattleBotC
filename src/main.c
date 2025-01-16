3e94d546bc7ca5b668c6d0b1a42a64d414994a9d
lucas_broda <lulubrod@gmail.com>
2025-01-16 11:27:32 +0100

    feat(main.c): ajout d'une fonction permettant de créer un objet représentant un joueur sur la map

--- src/main.c
+++ src/main.c
@@ -3,6 +3,26 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+BC_MapObject create_player_object(int id, BC_Vector3 position, BC_Vector3 speed, int health, int score, int armor) {
+    BC_PlayerData player_data;
+    player_data.id = id;
+    player_data.position = position;
+    player_data.speed = speed;
+    player_data.health = health;
+    player_data.score = score;
+    player_data.armor = armor;
+    player_data.is_dead = false;
+    
+    BC_MapObject player_object;
+    player_object.type = OT_PLAYER;
+    player_object.id = id;
+    player_object.position = position;
+    player_object.speed = speed;
+    player_object.health = health;
+
+    return player_object;
+}
+
 int main(int argc, char *argv[]) {
 
   BC_Connection *conn = bc_connect("5.135.136.236", 8080);
@@ -14,6 +34,8 @@ int main(int argc, char *argv[]) {
   BC_PlayerData data = bc_get_player_data(conn);
 
   BC_List *list = bc_radar_ping(conn);
+
+  create_player_object(1, data.position, data.speed, data.health, data.score, data.armor);
 
   do {
     BC_MapObject *map_object = (BC_MapObject *)bc_ll_value(list);
