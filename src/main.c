49ce6db98f8c4c6d8add1bd23f8651da6f514937
lucas_broda <lulubrod@gmail.com>
2025-02-03 13:29:18 +0100

    refactor(main.c): suppression d'éléments inutiles

--- src/main.c
+++ src/main.c
@@ -8,55 +8,6 @@
 #include <string.h>
 #include <math.h>
 
-
-// Fonction permettant d'afficher les données du joueur courant
-void print_data_current_player(BC_Connection *connection){
-  BC_PlayerData player = bc_get_player_data(connection);
-    printf("ID : %d\n", player.id);
-    printf("Vie du joueur : %d\n", player.health);
-    printf("Armure du joueur : %d\n", player.armor);
-    printf("Score : %d\n", player.score);
-    printf("Position x: %.2f\n", player.position.x);
-    printf("Position y: %.2f\n", player.position.y);
-    fflush(stdout);
-}
-
-// Fonction permettant de bouger le joueur
-void move_player(BC_Connection *connection, double x, double y, double z, float detection_perimeter, float player_x, float player_y){
-    int object_count = 0;
-
-    // Appel de la fonction radar pour vérifier les obstacles
-    ObjectInfo* objects = radar(connection, player_x, player_y, detection_perimeter, &object_count);
-
-    int obstacle_detected = 0;
-    for (int i = 0; i < object_count; i++) {
-        if (strcmp(objects[i].type, "WALL") == 0) {
-            float distance_x = objects[i].position_x - player_x;
-            float distance_y = objects[i].position_y - player_y;
-            if ((distance_x * x > 0) && (distance_y * y > 0)) {
-                obstacle_detected = 1;
-                break;
-            }
-        }
-    }
-
-    // Libérer la mémoire allouée
-    free(objects);
-
-    // Si un obstacle est détecté, inverser la direction
-    if (obstacle_detected) {
-        x = -x;
-        y = -y;
-        z = -z;
-        printf("Obstacle détecté, changement de direction\n");
-        fflush(stdout);
-    }
-
-    bc_set_speed(connection, x, y, z);
-    printf("Le joueur a bougé à la position x: %.2f, y: %.2f, z: %.2f\n", x, y, z);
-    fflush(stdout);
-}
-
 int main(int argc, char *argv[])
 {
 
