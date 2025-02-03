0078b236982b5ce93fd03cf080c427e8064353c6
lucas_broda <lulubrod@gmail.com>
2025-02-03 08:09:07 +0100

    feat(radar.h): ajout des enums/fonctions

--- include/radar.h
+++ include/radar.h
@@ -8,5 +8,9 @@ typedef struct {
     float position_y;
 } ObjectInfo;
 
+char* ConvertObjectTypeToString(enum BC_ObjectType type);
+
+// Fonction permettant de scanner les objets autour du joueur
+ObjectInfo* radar(BC_Connection *connection, float player_x, float player_y, float detection_radius_meters, int *count);
 
 #endif
